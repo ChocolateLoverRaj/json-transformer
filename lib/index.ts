feb9acb3337ef27ddf0f797923ab8ebd790925ab
Rajas Paranjpe <paranjperajas@gmail.com>
2021-03-12 16:21:47 -0600

    Export generate fn

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
+export { default as generate } from './generate'
+export { default as parser } from './parser'
 export { default as transform } from './transform'
 export { default as traverse } from './traverse'
-export { default as parser } from './parser'
